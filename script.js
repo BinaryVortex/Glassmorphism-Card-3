752a7395f30f5d6905e92c759b3d5b440a68e259
Disandu Perera <152260753+BinaryVortex@users.noreply.github.com>
2024-06-08 21:24:42 +0530

    Add files via upload
    Glassmorphism Card Built Using HTML,CSS And Javascript.

---
+++ script.js
@@ -0,0 +1,7 @@
+VanillaTilt.init(document.querySelectorAll(".card"), {
+		max: 25,
+        speed: 400,
+        easing:"cubic-bezier(.05,.80,.60,.99)",
+        perspective:500,
+        transition:true
+    });
